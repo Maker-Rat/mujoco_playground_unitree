542b5cffcbf82f85dd62a2704d7460d9190a22a9
Maker-Rat <ritwiksha2027@gmail.com>
2025-09-14 07:59:31 +0530

    minor fix

--- mujoco_playground/_src/locomotion/spiderbot/joystick.py
+++ mujoco_playground/_src/locomotion/spiderbot/joystick.py
@@ -174,10 +174,10 @@ class Joystick(spiderbot_base.SpiderbotEnv):
 
     # x=+U(-0.5, 0.5), y=+U(-0.5, 0.5), yaw=U(-3.14, 3.14).
     rng, key = jax.random.split(rng)
-    dxy = jax.random.uniform(key, (2,), minval=-0.5, maxval=0.5)
+    dxy = jax.random.uniform(key, (2,), minval=-0.0, maxval=0.0)
     qpos = qpos.at[0:2].set(qpos[0:2] + dxy)
     rng, key = jax.random.split(rng)
-    yaw = jax.random.uniform(key, (1,), minval=-3.14, maxval=3.14)
+    yaw = jax.random.uniform(key, (1,), minval=0.0, maxval=0.0)
     quat = math.axis_angle_to_quat(jp.array([0, 0, 1]), yaw)
     new_quat = math.quat_mul(qpos[3:7], quat)
     qpos = qpos.at[3:7].set(new_quat)
@@ -185,7 +185,7 @@ class Joystick(spiderbot_base.SpiderbotEnv):
     # d(xyzrpy)=U(-0.1, 0.1) - reduced range for more stable initialization
     rng, key = jax.random.split(rng)
     qvel = qvel.at[0:6].set(
-        jax.random.uniform(key, (6,), minval=-0.25, maxval=0.25)
+        jax.random.uniform(key, (6,), minval=-0.5, maxval=0.5)
     )
 
     # FIX: Use only the actuated joint positions for ctrl
